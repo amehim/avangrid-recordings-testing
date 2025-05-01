00ff919fecdfdf5458c9a23f25918aa5e1666f78
Idoga Ameh <pachiepcs@gmail.com>
2025-04-30 20:34:59 -0400

    Updating tls certificate

---
+++ terraform/cert-mgr.tf
@@ -0,0 +1,19 @@
+resource "kubernetes_manifest" "frontend_tls_certificate" {
+  manifest = {
+    "apiVersion" = "cert-manager.io/v1"
+    "kind"       = "Certificate"
+    "metadata" = {
+      "name"      = "frontend-tls"
+      "namespace" = "frontend"
+    }
+    "spec" = {
+      "secretName" = "frontend-tls"
+      "issuerRef" = {
+        "name" = "letsencrypt-prod"
+        "kind" = "ClusterIssuer"
+      }
+      "commonName" = var.frontend_domain
+      "dnsNames"   = [var.frontend_domain]
+    }
+  }
+}
